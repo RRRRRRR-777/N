f3398d686f54443acb6f01b754eb046bebda5f86
RikuYamada <r.yamada@portback.com>
2024-06-09 22:45:03 +0900

    fix: 細かいバグの修正

--- main.py
+++ main.py
@@ -8,58 +8,37 @@ start = time.time()
 init_process = InitProcess()
 logger = init_process.set_log()
 driver = init_process.set_selenium()
-# init_process.execute()
-# # finvizから銘柄の配列を取得 87.18s 1.45m
-# tmp_start = time.time()
-# pick_finviz = PickFinviz(logger)
-# pick_finviz.execute()
-# print(round(time.time() - tmp_start, 2))
-# # ヒストリカルデータをダウンロードする 3800.04s 63.33m
-# tmp_start = time.time()
-# hist_data = HistData(logger, driver)
-# hist_data.execute()
-# print(round(time.time() - tmp_start, 2))
-# # NASDAQのヒストリカルデータの列を増やす
-# tmp_start = time.time()
-# process_nasdaq = ProcessNASDAQ(logger)
-# process_nasdaq.execute()
-# print(round(time.time() - tmp_start, 2))
-# # 個別株のヒストリカルデータの列を増やす 610.45s 10.17m
-# tmp_start = time.time()
-# process_histdata = ProcessHistData(logger)
-# process_histdata.execute()
-# print(round(time.time() - tmp_start, 2))
-# # RSを計算する 91.94s 1.53m
-# tmp_start = time.time()
-# calculate_rs = CalculateRS(logger)
-# calculate_rs.execute()
-# print(round(time.time() - tmp_start, 2))
-# # BuyingStock.csvを出力する 0.3s
-# tmp_start = time.time()
-# buying_stock = BuyingStock(logger)
-# buying_stock.execute()
-# print(round(time.time() - tmp_start, 2))
-# CとAを取得する 2874.17s 47.90m
-tmp_start = time.time()
+init_process.execute()
+# finvizから銘柄の配列を取得 87.18s 1.45m
+pick_finviz = PickFinviz(logger)
+pick_finviz.execute()
+# ヒストリカルデータをダウンロードする 3800.04s 63.33m
+hist_data = HistData(logger, driver)
+hist_data.execute()
+# NASDAQのヒストリカルデータの列を増やす
+process_nasdaq = ProcessNASDAQ(logger)
+process_nasdaq.execute()
+# 個別株のヒストリカルデータの列を増やす 610.45s 10.17m
+process_histdata = ProcessHistData(logger)
+process_histdata.execute()
+# RSを計算する 91.94s 1.53m
+calculate_rs = CalculateRS(logger)
+calculate_rs.execute()
+# BuyingStock.csvを出力する 0.3s
+buying_stock = BuyingStock(logger)
+buying_stock.execute()
+# CとAを取得する 1881.23s 31.36m
 current_annual = CurrentAnnual(logger, driver)
 current_annual.execute()
-print(round(time.time() - tmp_start, 2))
-# 機関投資家の増加数を取得  2345.98s 39.09m
-tmp_start = time.time()
+# 機関投資家の増加数を取得  2345.98s 39.09m, 581.66s 240stocks
 institutional = Institutional(logger, driver)
 institutional.execute()
-print(round(time.time() - tmp_start, 2))
 # BuyingStock.csvに値を追加する 61.6s → 12s
-tmp_start = time.time()
 append_data = AppendData(logger)
 append_data.execute()
-print(round(time.time() - tmp_start, 2))
 # Excelに変換後視覚情報を調整 47.93s(セルの自動調整追加時)
-tmp_start = time.time()
 convert_excel = ConvertExcel(logger)
 convert_excel.execute()
-print(round(time.time() - tmp_start, 2))
-
 
 end = time.time()
 print(f"All Time : {round(end - start, 2)}")
